2b397cd024e7e9a486f0688942447a3dddfc2bf8
yuki.N <fairjmflyer@gmail.com>
2025-07-31 02:43:34 +0800

    add cjs export comment

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -10,3 +10,18 @@ export const apps = [
     },
   },
 ];
+// cjs if es export failed
+// module.exports = {
+//   apps: [
+//     {
+//       name: "ses-suppress-sync",
+//       script: "dist/index.js", // Run the compiled file
+//       watch: false,
+//       autorestart: true,
+//       max_memory_restart: "1G",
+//       env: {
+//         NODE_ENV: "production",
+//       },
+//     },
+//   ],
+// };
